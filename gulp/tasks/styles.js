34cb6203624173a636267f3f0e0540b98fbe7dee
Julia <kuchina@gmail.com>
2017-08-02 08:05:50 +1000

    new build

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    nested = require('postcss-nested'),
+    cssImport = require('postcss-import');
 
-gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
+gulp.task('styles', function(){
+  return  gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([cssImport, nested, autoprefixer]))
-    .on('error', function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
+    .on('error', function(errorInfo){
+    console.log(errorInfo.toString())
+    this.emit('end');
+  })
     .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,48 +1,35 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
-    browserSync = require('browser-sync').create(),
-    mocha = require('gulp-mocha'),
-    gutil = require('gulp-util');
+    browserSync = require('browser-sync').create();
 
 gulp.task('watch', function() {
-  browserSync.init({
-    notify: false,
-    server: {
-      baseDir: "app"
-    }
-  });
 
-  watch('./app/index.html', function() {
-    browserSync.reload();
-  });
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: "app"
+        }
+    });
 
-  watch('./app/assets/styles/**/*.css', function() {
-    gulp.start('cssInject');
-  });
+    watch('./app/index.html', function() {
+        browserSync.reload();
+    });
 
-  watch('./app/assets/scripts/**/*.js', function() {
-    gulp.start('scriptsRefresh');
-  });
-  
-  watch(['app/**', 'test/**'], function(){
-    gulp.start('mocha');
-  });
+    watch('./app/assets/styles/**/*.css', function() {
+        gulp.start('cssInject');
+    });
+
+    watch('./app/assets/scripts/**/*.js', function() {
+        gulp.start('scriptsRefresh');
+    })
 
 });
 
 gulp.task('cssInject', ['styles'], function() {
-  return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream());
+    return gulp.src('./app/temp/styles/styles.css')
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scriptsRefresh', ['scripts'], function() {
-  browserSync.reload();
-});
-
-gulp.task('mocha', function() {
-  return gulp.src(['test/*.js'], { read: false })
-    .pipe(mocha({ reporter: 'list',
-                  compilers: 'js:babel-core/register'
-                }))
-    .on('error', gutil.log);
+    browserSync.reload();
 });
